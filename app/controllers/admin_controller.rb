fcc0b072dd0e614101aad810d1294c769cb5b219
Cameron <cameron.p.buckingham@gmail.com>
2016-01-12 21:09:30 -0700

    github api is the best

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,8 @@ class AdminController < ApplicationController
 
   def show
     @coders = Coder.all
+    github = Github.new user: 'cpbuckingham', repo: 'codeblue_RoR'
+    @pr = github.pull_requests.list
     # @coder = Coder.find(params[:id])
     # pull_requests = @coder.pull_requests.year(current_year).order('created_at desc')
   end
--- app/models/coder.rb
+++ app/models/coder.rb
@@ -1,7 +1,3 @@
 class Coder < ActiveRecord::Base
 
-  def self.pull_requests
-    PullRequest.year(current_year).order('created_at desc').includes(:coder)
-  end
-
 end
